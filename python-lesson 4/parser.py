a1e536cb8c713578d5b7fbdf01337eaceb2148af
MurashovAA <MurashovAA@tmk-group.com>
2019-05-31 12:18:13 +0500

    lesson 4 done

--- python-lesson 4/parser.py
+++ python-lesson 4/parser.py
@@ -10,30 +10,48 @@ def get_html(url):
         print(r.status_code)
 
 def write_csv(data):
-    with open("yandx.csv", "a") as file:
+    with open("yandex.csv", "a") as file:
         writer = csv.writer(file)
-        pass
-
+        writer.writerow([
+            data["header"],
+            data["url"],
+            data["descr"]
+        ])
+                
 def get_page_data(html):
     soup = BeautifulSoup(html, "lxml")
     lis = soup.find_all("li", class_="serp-item")
+    print(len(lis))
     for li in lis:
         try:
-            header = li.find("div", class_="organic__url-text").text
+            header = li.find("div", class_="organic__url-text").text.strip()
         except:
             header = ""
         try:
             url = li.find("h2", class_="organic__title-wrapper typo typo_text_l typo_line_m").find("a").get("href")
         except:
             url = ""
-        print(header, url)
+        try:
+            descr = li.find("div", class_="text-container typo typo_text_m typo_line_m organic__text").text.strip()
+        except:
+            descr = ""
+
+        data = {
+            "header": header,
+            "url": url,
+            "descr": descr
+        }
+        write_csv(data)
+        # print(header, url, descr)
         
         
 
 
 def main():
-    url = "https://yandex.ru/search/?lr=21726&text=yandex&p=0"
-    get_page_data(get_html(url))
+    # for i in range(0, 10):
+        pattern = "https://yandex.ru/search/?lr=21726&text=yandex&p=0"
+        get_page_data(get_html(pattern))
+
 
 if __name__ == "__main__":
     main()
