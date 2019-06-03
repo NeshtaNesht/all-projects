30774a8af1cdd6140783290666321a862eed578c
MurashovAA <MurashovAA@tmk-group.com>
2019-06-03 07:36:27 +0500

    Тест

--- Оптовый_склад_Wivichan/Оптовый_склад_Wivichan/FUser.Designer.cs
+++ Оптовый_склад_Wivichan/Оптовый_склад_Wivichan/FUser.Designer.cs
@@ -42,6 +42,8 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -98,7 +100,7 @@
             // добавитьToolStripMenuItem
             // 
             this.добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
-            this.добавитьToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.добавитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.добавитьToolStripMenuItem.Text = "Добавить";
             // 
             // расходToolStripMenuItem
@@ -121,9 +123,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(12, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(325, 22);
+            this.label1.Size = new System.Drawing.Size(354, 22);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Информация о последних операциях";
+            this.label1.Text = "Информация о последних операциях за:";
             // 
             // btnCreateReport
             // 
@@ -138,7 +140,7 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(343, 44);
+            this.radioButton1.Location = new System.Drawing.Point(348, 66);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(62, 17);
             this.radioButton1.TabIndex = 4;
@@ -149,7 +151,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(343, 67);
+            this.radioButton2.Location = new System.Drawing.Point(348, 89);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(61, 17);
             this.radioButton2.TabIndex = 5;
@@ -159,16 +161,35 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 90);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 115);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(700, 445);
+            this.dataGridView1.Size = new System.Drawing.Size(700, 420);
             this.dataGridView1.TabIndex = 6;
+            // 
+            // dateTime
+            // 
+            this.dateTime.Location = new System.Drawing.Point(368, 44);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(163, 20);
+            this.dateTime.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(416, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 43);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Показать";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(724, 547);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTime);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -206,5 +227,7 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem1;
+        private System.Windows.Forms.DateTimePicker dateTime;
+        private System.Windows.Forms.Button button1;
     }
 }
