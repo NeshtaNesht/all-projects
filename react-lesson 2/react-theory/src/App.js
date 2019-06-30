1445037640a295906a9aa45adaddadd3f5c09255
MurashovAA <MurashovAA@tmk-group.com>
2019-06-30 15:52:02 +0500

    react

--- react-lesson 2/react-theory/src/App.js
+++ react-lesson 2/react-theory/src/App.js
@@ -1,24 +1,80 @@
-import React, { Component } from 'react';
-import './App.css';
-import Car from './Car/Car.js'
+import React, { Component } from "react";
+import "./App.css";
+import Car from "./Car/Car.js";
 
 class App extends Component {
+  state = {
+    cars: [
+      { name: "Ford Focus 3", year: "2018" },
+      { name: "Audi A3", year: "2007" },
+      { name: "Mazda 3", year: "2007" }
+    ],
+    pageTitle: "Автомобили",
+    showCars: false
+  };
+
+  OnChangeName = (name, index) => {
+    const car = this.state.cars[index];
+    car.name = name;
+    //Клонируем массив с помощью спред-оператора ... из state в cars, чтобы не было мутации
+    const cars = [...this.state.cars];
+    cars[index] = car;
+    this.setState({
+      cars //Упрощенная запись из-за одинаковых наименований
+    });
+  };
+
+  OnDeleteCar(index){
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
+  }
+
+  showCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    });
+  };
+
   render() {
     const divStyle = {
-      margin: '10px',
-      border: '1px solid #ccc',
-      padding: '20px'
-    }
+      margin: "10px",
+      border: "1px solid #ccc",
+      padding: "20px"
+    };
+
+    let nameButton = this.state.showCars ? "HIDE" : "SHOW";
+    let cars = this.state.showCars
+      ? this.state.cars.map((car, index) => {
+          return (
+            <Car
+              key={index}
+              name={car.name}
+              year={car.year}
+              onChangeName={event => {
+                this.OnChangeName(event.target.value, index);
+              }}
+              delete={
+                this.OnDeleteCar.bind(this, index)
+              }
+            />
+          );
+        })
+      : null;
     return (
       <div className="App" style={divStyle}>
-        <h2>Hello, World, this is class App</h2>
-        <Car name="Ford Focus 3" year="2018" />
-        <Car name="Audi A3" year="2007" >
-          <p style={{color: 'red'}}>COLOR</p>
-        </Car>
-        <Car name="Mazda 3" year="2007" />
+        <h2>{this.state.pageTitle}</h2>
+        <div style={{
+          width: 400,
+          margin: 'auto',
+          paddingTop: '10px'          
+        }}>
+          {cars}
+        </div>        
+        <button onClick={this.showCarsHandler}>{nameButton}</button>
       </div>
-    )
+    );
   }
 }
 
--- react-lesson 2/react-theory/src/Car/Car.js
+++ react-lesson 2/react-theory/src/Car/Car.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default (props) => (
-    <div>
+    <div style={{
+        border: '1px solid #ccc',
+        marginBottom: '5px',
+        display: 'inline-block',
+        padding: '10px',
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        borderRadius: '5px'        
+    }}>
         <h3>Car name: {props.name}</h3>
         <p>Year: {props.year}</p>
         {props.children}
+        <input type="text" onChange={props.onChangeName} value={props.name} />
+        <button onClick={props.delete}>Delete</button>
     </div>
 )
